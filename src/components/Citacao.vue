0da224d8b7798c966096785f5696a8ccab25193a
Vlary <manoelfranciscose@gmail.com>
2021-01-08 17:26:52 -0300

    citacoes-exercise

---
+++ src/components/Citacao.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="citacao">
+        <slot name="fonte"></slot>
+        <slot> </slot>
+        <div class="autor"><slot name="autor"></slot></div>       
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    created(){
+        console.log('created')
+    }
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
---
+++ src/components/Sobre.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <p class="nome">Citações</p>
+        
+        <p class="versao"> Manoel</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        border: 1px solid #DDD;
+        height: 140px;
+        width: 140px;
+        border-radius: 100px;
+        
+    }
+
+    p {
+        margin: 0;
+    }
+
+    .nome {
+        font-size: 1.1rem;
+        font-weight: bold;
+    }
+
+    .versao {
+        font-size: 1.4rem;
+    }
+</style>
